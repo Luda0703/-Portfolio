8ed982d220ba2ec0bc0c44e90334e26c5b69b691
Nikitenko Ludmila <117203301+Luda0703@users.noreply.github.com>
2023-11-13 16:22:01 +0200

    router

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,27 @@
 import './styles/main.css';
 
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 import Navbar from 'components/navbar/Navbar';
 import Footer from 'components/footer/Footer';
-// import Home from 'pages/Home';
+import Project from 'pages/Project';
+import Home from 'pages/Home';
 import Projects from 'pages/Prodjects';
-// import Contacts from 'pages/Contacts';
+import Contacts from 'pages/Contacts';
 
 export const App = () => {
   return (
     <div className="App">
-      <Navbar />
-      {/* <Home /> */}
-      <Projects/>
-      {/* <Contacts/> */}
-      <Footer />
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/project" element={<Project />} />
+          <Route path="/contacts" element={<Contacts />} />
+        </Routes>
+        <Footer />
+      </Router>
     </div>
   );
 };
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,16 +1,21 @@
 import sun from './../../img/icons/sun.svg';
 import moon from './../../img/icons/moon.svg';
+import { NavLink } from 'react-router-dom';
 
 import './style.css'
 
 const Navbar = () => {
+  const activLinc = 'nav-list__link nav-list__link--active';
+  const normalLinc = 'nav-list__link';
+
+
   return (
     <nav className="nav">
       <div className="container">
         <div className="nav-row">
-          <a href="./index.html" className="logo">
-            Portfolio
-          </a>
+          <NavLink to='/' className="logo">
+          Portfolio
+          </NavLink>
 
           <button className="dark-mode-btn">
             <img src={sun} alt="Light mode" className="dark-mode-btn__icon" />
@@ -19,22 +24,25 @@ const Navbar = () => {
 
           <ul className="nav-list">
             <li className="nav-list__item">
-              <a
-                href="./index.html"
-                className="nav-list__link nav-list__link--active"
-              >
-                Home
-              </a>
+              <NavLink to='/' className={({isActive}) => {
+                return isActive ? activLinc : normalLinc
+              }}>
+              Home
+              </NavLink>
             </li>
             <li className="nav-list__item">
-              <a href="./projects.html" className="nav-list__link">
-                Projects
-              </a>
+              <NavLink to='/projects' className={({isActive}) => {
+                return isActive ? activLinc : normalLinc
+              }}>
+              Projects
+              </NavLink>
             </li>
             <li className="nav-list__item">
-              <a href="./contacts.html" className="nav-list__link">
-                Contacts
-              </a>
+              <NavLink to='/contacts' className={({isActive}) => {
+                return isActive ? activLinc : normalLinc
+              }}>
+              Contacts
+              </NavLink>
             </li>
           </ul>
         </div>
