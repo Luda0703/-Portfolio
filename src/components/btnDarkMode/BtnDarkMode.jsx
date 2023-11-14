f81e175a83a61bf03e0df69b203b5e9483f606aa
Nikitenko Ludmila <117203301+Luda0703@users.noreply.github.com>
2023-11-14 11:58:22 +0200

    localStorage

--- src/components/btnDarkMode/BtnDarkMode.jsx
+++ src/components/btnDarkMode/BtnDarkMode.jsx
@@ -1,10 +1,12 @@
-import { useState, useEffect, useRef } from 'react';
+import { useEffect, useRef } from 'react';
+import { useLocalStorage } from 'utils/useLocalStorage';
 import './style.css';
 import sun from './../../img/icons/sun.svg';
 import moon from './../../img/icons/moon.svg';
 
 const BtnDarkMode = () => {
-    const [darkMode, setDarkMode] = useState('light');
+    const [darkMode, setDarkMode] = useLocalStorage('darkMode', 'light');
+
     const btnRef = useRef(null);
 
     useEffect(() => {
---
+++ src/utils/useLocalStorage.js
@@ -0,0 +1,28 @@
+import { useState, useEffect } from 'react';
+
+function getStorageValue(key, defaultValue) {
+try {
+    const saved = localStorage.getItem(key);
+    const initial = JSON.parse(saved);
+    return initial || defaultValue;
+  } catch (error) {
+    console.error('Error parsing JSON:', error);
+    return defaultValue;
+  }
+}
+
+export const useLocalStorage = (key, defaultValue) => {
+  const [value, setValue] = useState(() => {
+    return getStorageValue(key, defaultValue);
+  });
+
+  useEffect(() => {
+    try {
+        localStorage.setItem(key, JSON.stringify(value));
+      } catch (error) {
+        console.error('Error storing in localStorage:', error);
+      }
+  }, [key, value]);
+
+  return [value, setValue];
+};
