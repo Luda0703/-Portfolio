d58bc802273d38a6f11fb2b067f9cebb4b76913a
Nikitenko Ludmila <117203301+Luda0703@users.noreply.github.com>
2023-11-14 11:21:02 +0200

    btnDarkMode

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,8 +1,8 @@
-import sun from './../../img/icons/sun.svg';
-import moon from './../../img/icons/moon.svg';
+
 import { NavLink } from 'react-router-dom';
 
 import './style.css'
+import BtnDarkMode from 'components/btnDarkMode/BtnDarkMode';
 
 const Navbar = () => {
   const activLinc = 'nav-list__link nav-list__link--active';
@@ -17,10 +17,7 @@ const Navbar = () => {
           Portfolio
           </NavLink>
 
-          <button className="dark-mode-btn">
-            <img src={sun} alt="Light mode" className="dark-mode-btn__icon" />
-            <img src={moon} alt="Dark mode" className="dark-mode-btn__icon" />
-          </button>
+          <BtnDarkMode/>
 
           <ul className="nav-list">
             <li className="nav-list__item">
