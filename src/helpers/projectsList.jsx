fd90acff5f76cd7ccd51909744243e8a9bbce522
Nikitenko Ludmila <117203301+Luda0703@users.noreply.github.com>
2024-02-23 10:08:51 +0200

    update

--- src/helpers/projectsList.jsx
+++ src/helpers/projectsList.jsx
@@ -4,6 +4,7 @@ import project_03 from './../img/projects/03.jpg';
 import project_04 from './../img/projects/04.jpg';
 import project_05 from './../img/projects/05.jpg';
 import project_06 from './../img/projects/06.jpg';
+import project_07 from './../img/projects/07.jpg';
 
 export const projects = [
   {
@@ -13,33 +14,39 @@ export const projects = [
     gitHubLink: 'https://luda0703.github.io/goit-markup-hw-07/',
   },
   {
-    title: 'Ice Creame',
+    title: 'Candlers shop',
     skills: 'html, css, js',
     img: project_02,
+    gitHubLink: 'https://luda0703.github.io/project-html-css-js/',
+  },
+  {
+    title: 'Ice Creame',
+    skills: 'html, css, js',
+    img: project_03,
     gitHubLink: 'https://voloshun555.github.io/Ice-cream-comand11/',
   },
   {
     title: 'Bookshelf',
     skills: 'html, css, js',
-    img: project_03,
+    img: project_04,
     gitHubLink: 'https://tien1o.github.io/bookshelf-group-09/',
   },
   {
     title: 'HealthyHub',
     skills: 'html, scss, js, react, redax, node.js',
-    img: project_04,
+    img: project_05,
     gitHubLink: 'https://rentensei.github.io/HealthyHub/',
   },
   {
     title: 'Snake Game',
     skills: 'html, css, js, typeScript, react, node.js, postgreSQL',
-    img: project_05,
+    img: project_06,
     gitHubLink: 'https://luda0703.github.io/Snake/',
   },
   {
     title: 'Movie',
     skills: 'css, js, react',
-    img: project_06,
+    img: project_07,
     gitHubLink: 'https://luda0703.github.io/goit-react-hw-05-movies/',
   },
 ];
